8f45f2dc2b77dbe9be29a7cf46d43674f970e26a
Evans Rutto <robbyevans001@gmail.com>
2022-06-16 22:23:26 +0300

    commit tree 30a07cba9444ac97ba041426c563790fcd84c88b

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 function isPalindrome(word) {
   // Write your algorithm here
+    // create an array from the input string
+    const wordArray = word.split("");
+    // reverse the array
+    const reversedWordArray = wordArray.reverse();
+    // create a string from the reversed array
+    const reversedWord = reversedWordArray.join("");
+    // return the reversed string
+    return (reversedWord===word);
 }
 
 /* 
   Add your pseudocode here
 */
+//steps
+//1*make a function called isPalindrom
+//2*make a const called arrayString
+//3*slplit our word using .split('') and assign to arraystring
+//4*use .reverse () propery to reverse our word
+//5*assign the reversed word to a new const called joinedArray
+//7*return joinedArray 
+//6* compare joinedArray to arrayString using strict equaliy operator
 
 /*
   Add written explanation of your solution here
+
+  // using javascript inbuilt function string.reverse() to change our string to and return itbackwards
 */
 
 // You can run `node index.js` to view these console logs
